bab3f4454c5acb3a8a93e31b642df221fc6ef5e7
cdcleones <47822766+cdcleones@users.noreply.github.com>
2019-04-15 20:37:51 +0200

    Add files via upload

--- js/races.js
+++ js/races.js
@@ -131,13 +131,16 @@ function ProcesaAnio(anio){
 			  $('#btnLG2R8').click(function(){ if ($('#LG2R8').is (':hidden')){ $('#LV1R8').hide(); $('#LV1N8').hide(); $('#LV2R8').hide(); $('#LV3R8').hide(); $('#LJ1N8').hide(); $('#LMFN8').hide(); $('#LJ2N8').hide(); $('#LV5R8').hide(); $('#LF1N8').hide(); $('#LF1R8').hide(); $('#LF2R8').hide(); $('#LF2N8').hide(); $('#LG1R8').hide(); $('#LG1N8').hide(); $('#LG2N8').hide(); $('#LG2R8').show(); } else $('#LG2R8').hide(); });
 			  
 			  /* 2019 */
-			  $('#btnLV1R9').click(function(){ if ($('#LV1R9').is (':hidden')){ $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV1R9').show(); } else $('#LV1R9').hide(); });
-			  $('#btnLV2R9').click(function(){ if ($('#LV2R9').is (':hidden')){ $('#LV1R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV2R9').show(); } else $('#LV2R9').hide(); });
-			  $('#btnLV1N9').click(function(){ if ($('#LV1N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV1N9').show(); } else $('#LV1N9').hide(); });
-			  $('#btnLJ1N9').click(function(){ if ($('#LJ1N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LJ1N9').show(); } else $('#LJ1N9').hide(); });
-			  $('#btnLV2N9').click(function(){ if ($('#LV2N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV3R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV2N9').show(); } else $('#LV2N9').hide(); });
-			  $('#btnLV3R9').click(function(){ if ($('#LV3R9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV3R9').show(); } else $('#LV3R9').hide(); });
-			  
+			  $('#btnLV1R9').click(function(){ if ($('#LV1R9').is (':hidden')){ $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV1R9').show(); } else $('#LV1R9').hide(); });
+			  $('#btnLV2R9').click(function(){ if ($('#LV2R9').is (':hidden')){ $('#LV1R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV2R9').show(); } else $('#LV2R9').hide(); });
+			  $('#btnLV1N9').click(function(){ if ($('#LV1N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV1N9').show(); } else $('#LV1N9').hide(); });
+			  $('#btnLJ1N9').click(function(){ if ($('#LJ1N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LJ1N9').show(); } else $('#LJ1N9').hide(); });
+			  $('#btnLV2N9').click(function(){ if ($('#LV2N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV2N9').show(); } else $('#LV2N9').hide(); });
+			  $('#btnLV3R9').click(function(){ if ($('#LV3R9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV3R9').show(); } else $('#LV3R9').hide(); });
+			  $('#btnLV4R9').click(function(){ if ($('#LV4R9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LJ2N9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV4R9').show(); } else $('#LV4R9').hide(); });
+			  $('#btnLJ2N9').click(function(){ if ($('#LJ2N9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LV5R9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LJ2N9').show(); } else $('#LJ2N9').hide(); });
+			  $('#btnLV5R9').click(function(){ if ($('#LV5R9').is (':hidden')){ $('#LV1R9').hide(); $('#LV2R9').hide(); $('#LV1N9').hide(); $('#LJ1N9').hide(); $('#LV2N9').hide(); $('#LV3R9').hide(); $('#LV4R9').hide(); $('#LJ2N9').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#').hide(); $('#LV5R9').show(); } else $('#LV5R9').hide(); });
+
 			  /* 2020 */
 			  
 			  crearMejoresPalomasNacional();
@@ -399,7 +402,8 @@ function crearMejoresPalomasNacional(){
 			"previous": "Anterior"
 		},
 		"info": "Mostrando página _PAGE_ de _PAGES_",
-		"infoFiltered": "(Filtrando de un total de _MAX_ registros)"
+		"infoFiltered": "(Filtrando de un total de _MAX_ registros)",
+		"zeroRecords": "No se han encontrado datos"
 	},
 	columnDefs: [
 		{ targets: [0,5], className: 'dt-body-right' }
@@ -520,7 +524,8 @@ function crearMejoresPalomasRegional(){
 			"previous": "Anterior"
 		},
 		"info": "Mostrando página _PAGE_ de _PAGES_",
-		"infoFiltered": "(Filtrando de un total de _MAX_ registros)"
+		"infoFiltered": "(Filtrando de un total de _MAX_ registros)",
+		"zeroRecords": "No se han encontrado datos"
 	},
 	columnDefs: [
 		{ targets: [0,5], className: 'dt-body-right' }
@@ -553,23 +558,22 @@ function ProcesarAnilla(anilla,anio) {
 		  }
 		}
 		var htmlAnillas="";
-		htmlAnillas="<div class='infocarrera'>";
-		htmlAnillas+="<div class='row'>";
-		htmlAnillas+="<h3>Anilla 16-123456</h3>";
-		htmlAnillas+="</div>";
-		htmlAnillas+="<div class='row'>";
-		htmlAnillas+="<div class='col-md-4'>Propietario: JESÚS ROBERTO GARCÍA GARCIA</div>";
-		htmlAnillas+="<div class='col-md-4'>Color: Rodao ali</div>";
-		htmlAnillas+="<div class='col-md-4'>Sexo: Hembra</div>";
-		htmlAnillas+="</div>";
-		htmlAnillas+="</div>";
-		htmlAnillas+="<div><canvas id='myChart'></canvas></div>";
-		
-		html=$.parseHTML(htmlAnillas,true);
-		$clave=$('#infoanilla');
-		$clave.empty();
-		$clave.append(html);
-		//escribeChart();
+		if (existeAnilla(anio,anilla)==true){
+		  htmlAnillas=getDatosAnilla(anio,anilla);
+		  html=$.parseHTML(htmlAnillas,true);
+		  $clave=$('#infoanilla');
+		  $clave.empty();
+		  $clave.append(html);
+		  escribeChartNacional(anilla,anio);
+		  escribeChartRegional(anilla,anio);
+		}
+		else{
+		  htmlAnillas="<br />Anilla no encontrada";
+		  html=$.parseHTML(htmlAnillas,true);
+		  $clave=$('#infoanilla');
+		  $clave.empty();
+		  $clave.append(html);
+		}
 	  });
 	}); //anillas
   } else {
@@ -606,27 +610,27 @@ function ProcesarAnilla(anilla,anio) {
 				  req3.done(function(response){
 					for (var i=0; i<datosAnillas.length; i++){
 					  if (datosAnillas[i].anio==anio){
-						var nuevo3 = {anio:datosAnillas[i].anio,club:datosAnillas[i].club,concurso:datosAnillas[i].concurso,anilla:datosAnillas[i].anilla,pais:datosAnillas[i].pais,sexo:datosAnillas[i].sexo,color:datosAnillas[i].color,socio:datosAnillas[i].socio};
+						var nuevo3 = {anio:datosAnillas[i].anio,club:datosAnillas[i].club,concurso:datosAnillas[i].concurso,anilla:datosAnillas[i].anilla,pais:datosAnillas[i].pais,sexo:datosAnillas[i].sexo,color:datosAnillas[i].color,nombre:datosAnillas[i].nombre};
 						arrAnillas.push(nuevo3);
 					  }
 					}
 					var htmlAnillas="";
-					htmlAnillas="<div class='infocarrera'>";
-					htmlAnillas+="<div class='row'>";
-					htmlAnillas+="<h3>Anilla 16-123456</h3>";
-					htmlAnillas+="</div>";
-					htmlAnillas+="<div class='row'>";
-					htmlAnillas+="<div class='col-md-4'>Propietario: JESÚS ROBERTO GARCÍA GARCIA</div>";
-					htmlAnillas+="<div class='col-md-4'>Color: Rodao ali</div>";
-					htmlAnillas+="<div class='col-md-4'>Sexo: Hembra</div>";
-					htmlAnillas+="</div>";
-					htmlAnillas+="</div>";
-					htmlAnillas+="<div><canvas id='myChart'></canvas></div>";
-					html=$.parseHTML(htmlAnillas,true);
-					$clave=$('#infoanilla');
-					$clave.empty();
-					$clave.append(html);
-					//escribeChart();
+					if (existeAnilla(anio,anilla)==true){
+					  htmlAnillas=getDatosAnilla(anio,anilla);
+					  html=$.parseHTML(htmlAnillas,true);
+					  $clave=$('#infoanilla');
+					  $clave.empty();
+					  $clave.append(html);
+					  escribeChartNacional(anilla,anio);
+					  escribeChartRegional(anilla,anio);
+					}
+					else{
+					  htmlAnillas="<br />Anilla no encontrada";
+					  html=$.parseHTML(htmlAnillas,true);
+					  $clave=$('#infoanilla');
+					  $clave.empty();
+					  $clave.append(html);
+					}
 				  });
 				}); //anillas
 
@@ -643,11 +647,139 @@ function ProcesarAnilla(anilla,anio) {
   
 }
 
-function escribeChart(){
-  Chart.defaults.global.elements.line.fill = false;
+function existeAnilla(anio,anilla){
+  for (var i=0; i<arrAnillas.length; i++){
+	if ((arrAnillas[i].anio==anio)&&(arrAnillas[i].anilla==anilla)){
+	  return true;
+	  break;
+	}
+  }
+  return false;
+}
 
+function getDatosAnilla(anio,anilla){
+  var propietario="";
+  var color="";
+  var sexo="";
+  var pais="";
+  var datosAnilla="";
+  for (var i=0; i<arrAnillas.length; i++){
+	if ((arrAnillas[i].anio==anio)&&(arrAnillas[i].anilla==anilla)){
+	  propietario=arrAnillas[i].nombre;
+	  color=arrAnillas[i].color;
+	  sexo=arrAnillas[i].sexo;
+	  pais=arrAnillas[i].pais;
+	  break;
+	}
+  }
+  datosAnilla="<div class='infocarrera'>";
+  datosAnilla+="<div class='row'>";
+  datosAnilla+="<h3>Anilla "+anilla+"</h3>";
+  datosAnilla+="</div><br />";
+  datosAnilla+="<div class='row'>";
+  datosAnilla+="<div class='col-md-6 izda'>Propietario: " + propietario + "</div>";
+  datosAnilla+="<div class='col-md-6 izda'>Pais: " + pais + "</div>";
+  datosAnilla+="<div class='col-md-6 izda'>Color: " + color + "</div>";
+  datosAnilla+="<div class='col-md-6 izda'>Sexo: " + sexo + "</div>";
+  datosAnilla+="</div>";
+  datosAnilla+="</div>";
+  datosAnilla+="<h3>Nacional</h3>";
+  datosAnilla+="<div><canvas id='myChartNacional'></canvas></div>";
+  datosAnilla+="<br /><h3>Regional</h3>";
+  datosAnilla+="<div><canvas id='myChartRegional'></canvas></div>";
+  return datosAnilla;
+}
+
+function dimeCodigos(esNacional){
+  var arrCodigos=[];
+  var j=0;
+  for (var i=0; i<arrConcursos.length; i++){
+	if (arrConcursos[i].activo==true){
+	  if (esNacional==true){ //Nacional
+		if (arrConcursos[i].codigo.charAt(arrConcursos[i].codigo.length-2)=='N'){
+		  arrCodigos[j++]=arrConcursos[i].codigo;
+		}
+	  }
+	  else{ //Regional
+		if (arrConcursos[i].codigo.charAt(arrConcursos[i].codigo.length-2)!='N'){
+		  arrCodigos[j++]=arrConcursos[i].codigo;
+		}
+	  }
+	}
+  }
+  return arrCodigos;
+}
+
+function dimePosiciones(anilla,arrCodigos){
+  var arrPosiciones=[];
+  for (var i=0; i<arrCodigos.length; i++){
+	var encontrada=false;
+	for (var j=0; j<arrCarreras.length; j++){
+	  if ((arrCarreras[j].concurso==arrCodigos[i])&&(arrCarreras[j].anilla==anilla)){
+		encontrada=true;
+		break
+	  }
+	}
+	if (encontrada==true){
+	  arrPosiciones[i]=arrCarreras[j].pos;
+	}
+	else{
+	  arrPosiciones[i]=null;
+	}
+  }
+  return arrPosiciones;
+}
+
+function dimeClasificadas(esNacional){
+  var arrClasificadas=[];
+  var j=0;
+  for (var i=0; i<arrConcursos.length; i++){
+	if (arrConcursos[i].activo==true){
+	  if (esNacional==true){ //Nacional
+		if (arrConcursos[i].codigo.charAt(arrConcursos[i].codigo.length-2)=='N'){
+		  arrClasificadas[j++]=arrConcursos[i].clasif;
+		}
+	  }
+	  else{ //Regional
+		if (arrConcursos[i].codigo.charAt(arrConcursos[i].codigo.length-2)!='N'){
+		  arrClasificadas[j++]=arrConcursos[i].clasif;
+		}
+	  }
+	}
+  }
+  return arrClasificadas;
+}
+
+function dimeNoClasificadas(esNacional){
+  var arrNoClasificadas=[];
+  var j=0;
+  for (var i=0; i<arrConcursos.length; i++){
+	if (arrConcursos[i].activo==true){
+	  if (esNacional==true){ //Nacional
+		if (arrConcursos[i].codigo.charAt(arrConcursos[i].codigo.length-2)=='N'){
+		  arrNoClasificadas[j++]=arrConcursos[i].noclasif;
+		}
+	  }
+	  else{ //Regional
+		if (arrConcursos[i].codigo.charAt(arrConcursos[i].codigo.length-2)!='N'){
+		  arrNoClasificadas[j++]=arrConcursos[i].noclasif;
+		}
+	  }
+	}
+  }
+  return arrNoClasificadas;
+}
+
+function escribeChartRegional(anilla,anio){
+  Chart.defaults.global.elements.line.fill = false;
+  var arrCodigos=dimeCodigos(false);
+  var arrPosiciones=dimePosiciones(anilla,arrCodigos);
+  var arrEncestada=dimeEncestada(anilla,anio,arrCodigos);
+  var arrClasificadas=dimeClasificadas(false);
+  var arrNoClasificadas=dimeNoClasificadas(false);
+  //data: [0,0,0,0,0,0,0,0,0]
   var barChartData = {
-	labels: ["LV1R8","LV2R8","LV3R8","LV4R8","LV5R8","LF1R8","LF2R8","LG1R8","LG2R8"],
+	labels: arrCodigos,
 	datasets: [{
 	  type: 'line',
 	  label: 'Posición',
@@ -656,31 +788,31 @@ function escribeChart(){
 	  borderWidth: 5,
 	  backgroundColor: "rgba(0,0,255,0.5)",
 	  borderColor: "rgba(0,0,255,0.5)",
-	  data: [43,15,123,,,7,6,6,1]
+	  data: arrPosiciones
 	}, {
 	  type: 'bubble',
 	  label: 'Encestada',
 	  id: "y-axis-1",
-	  borderWidth: 4,
+	  borderWidth: 8,
 	  backgroundColor: "rgba(255,0,0,0.7)",
 	  borderColor: "rgba(255,0,0,0.7)",
-	  data: [0,0,0,,0,0,0,0,0]
+	  data: arrEncestada
 	}, {
 	  type: 'bar',
 	  label: 'Clasificadas',
 	  id: "y-axis-0",
 	  backgroundColor: "rgba(0,198,79,0.75)",
-	  data: [143,115,223,100,197,67,76,66,51]
+	  data: arrClasificadas
 	}, {
 	  type: 'bar',
 	  label: 'No clasificadas',
 	  id: "y-axis-0",
 	  backgroundColor: "rgba(187,187,187,0.75)",
-	  data: [7,5,25,2,3,0,4,1,8]
+	  data: arrNoClasificadas
 	}]
   };
 
-  var ctx = document.getElementById("myChart").getContext("2d");
+  var ctx = document.getElementById("myChartRegional").getContext("2d");
   var ch = new Chart(ctx, {
 	type: 'bar',
 	data: barChartData,
@@ -690,7 +822,7 @@ function escribeChart(){
 	  title: {
 		display: true,
 		padding: 10,
-		text: ['Anilla 16-124613','Año 2018'],
+		text: [anilla,anio],
 		position: 'top'
 	  },
 	  tooltips: {
@@ -734,4 +866,135 @@ function escribeChart(){
 	  
 	}
   });
+}
+
+function escribeChartNacional(anilla,anio){
+  Chart.defaults.global.elements.line.fill = false;
+  var arrCodigos=dimeCodigos(true);
+  var arrPosiciones=dimePosiciones(anilla,arrCodigos);
+  var arrEncestada=dimeEncestada(anilla,anio,arrCodigos);
+  var arrClasificadas=dimeClasificadas(true);
+  var arrNoClasificadas=dimeNoClasificadas(true);
+  //data: [0,0,0,0,0,0,0,0,0]
+  var barChartData = {
+	labels: arrCodigos,
+	datasets: [{
+	  type: 'line',
+	  label: 'Posición',
+	  id: "y-axis-1",
+	  lineTension: 0,
+	  borderWidth: 5,
+	  backgroundColor: "rgba(0,0,255,0.5)",
+	  borderColor: "rgba(0,0,255,0.5)",
+	  data: arrPosiciones
+	}, {
+	  type: 'bubble',
+	  label: 'Encestada',
+	  id: "y-axis-1",
+	  borderWidth: 8,
+	  backgroundColor: "rgba(255,0,0,0.7)",
+	  borderColor: "rgba(255,0,0,0.7)",
+	  data: arrEncestada
+	}, {
+	  type: 'bar',
+	  label: 'Clasificadas',
+	  id: "y-axis-0",
+	  backgroundColor: "rgba(0,198,79,0.75)",
+	  data: arrClasificadas
+	}, {
+	  type: 'bar',
+	  label: 'No clasificadas',
+	  id: "y-axis-0",
+	  backgroundColor: "rgba(187,187,187,0.75)",
+	  data: arrNoClasificadas
+	}]
+  };
+
+  var ctx = document.getElementById("myChartNacional").getContext("2d");
+  var ch = new Chart(ctx, {
+	type: 'bar',
+	data: barChartData,
+	options: {
+	  animation: true,
+	  //responsive: true,
+	  title: {
+		display: true,
+		padding: 10,
+		text: [anilla,anio],
+		position: 'top'
+	  },
+	  tooltips: {
+		mode: 'index'
+	  },
+	  legend: {
+		//position: 'right'
+	  },
+	  showTooltips: true,
+	  scales: {
+		xAxes: [{
+		  stacked: true
+		}],
+		yAxes: [{
+		  stacked: true,
+		  position: "left",
+		  id: "y-axis-0",
+		}]
+	  },
+	  animation: {
+		//duration: 0,
+		onComplete: function () {
+		  
+		  var ctx = this.chart.ctx;
+		  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
+		  ctx.textAlign = 'center';
+		  ctx.textBaseline = 'bottom';
+		  ctx.fillStyle = "#000";
+		  this.data.datasets.forEach(function (dataset) {
+			if (dataset.label == 'Posición'){
+			  for (var i = 0; i < dataset.data.length; i++) {
+				for (var key in dataset._meta) {
+				  var model = dataset._meta[key].data[i]._model;
+				  ctx.fillText(dataset.data[i] + 'ª', model.x, model.y - 10);
+				}
+			  }
+			}
+		  });
+		}
+	  }
+	  
+	}
+  });
+}
+
+function RellenaAnios(){
+  for (i=0; i<arrAnios.length;i++){
+	if (arrAnios[i].activo == true){
+	  document.getElementById("anioconcurso").options[document.getElementById("anioconcurso").options.length]=new Option(arrAnios[i].anio,arrAnios[i].anio);
+	}
+  }
+  var anioActual=new Date().getFullYear();
+  for (i=0; i<arrAnios.length;i++){
+	if (anioActual==arrAnios[i].anio){
+	  document.getElementById("anioconcurso").selectedIndex = i;
+	  return;
+	}
+  }
+}
+
+function dimeEncestada(anilla,anio,arrCodigos){
+  var arrEnceste=[];
+  for (var i=0; i<arrCodigos.length; i++){
+	var encontrada=false;
+	bucle1:
+	for (var j=0; j<arrAnillas.length; j++){
+	  if ((arrCodigos[i]==arrAnillas[j].concurso)&&(arrAnillas[j].anio==anio)&&(arrAnillas[j].anilla==anilla)){
+		encontrada=true;
+		break bucle1;
+	  }
+	}
+	if (encontrada==true){
+	  arrEnceste[i]=0;
+	}
+  }
+  return arrEnceste;
 }
